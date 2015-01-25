597c67c8fcfc94fcdf59545c88e15463e33514bb
Flowck II <flowck96@hotmail.com>
2015-01-25 13:11:25 +0100

    Change form on select interest

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ var BODY = document.getElementsByTagName("body"),
 	header = document.querySelector(".mainHeader");
 
 window.addEventListener("scroll", function() {
-	console.log(window.scrollY);
+	//console.log(window.scrollY);
 }, false);
 
-/* Formulário de inscrição */
+/* Lightbox */
 
 // DOM
 var fecharBT = document.querySelector(".fechar"),
@@ -22,3 +22,53 @@ inscrever_me.addEventListener("click", function(){
 fecharBT.addEventListener("click", function(){
 	lightbox.style.display = "none";
 }, false);
+
+/* Form */
+
+// DOM
+var select_interesse = document.querySelector(".interessado"),
+	proximo = document.querySelector(".proximo");
+
+select_interesse.addEventListener("change", function(){
+	
+	// Enable o button
+	proximo.disabled = false;
+
+}, false);
+
+
+proximo.addEventListener("click", function(){
+
+	newForm(select_interesse.value);
+
+}, false);
+
+var newForm = function(nomeDoForm){
+	// DOM
+	var startup = document.querySelector(".startup"),
+		investidor = document.querySelector(".investidor"),
+		mentor = document.querySelector(".mentor"),
+		formBasico = document.querySelector(".formBasico");
+
+	// Oculta o formulário básico
+	formBasico.style.display = "none";
+
+	// Mostra o form escolhido
+	if(nomeDoForm == "startup"){
+		startup.style.display = "block";
+	}
+
+	if(nomeDoForm == "investidor"){
+		investidor.style.display = "block";
+	}
+
+	if(nomeDoForm == "mentor"){
+		mentor.style.display = "block";
+	}
+
+	// Disable o botão
+	proximo.disabled = true;
+
+}
+
+console.log(proximo.disabled);
