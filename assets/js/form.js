f446b98c41c4dacf9e884901de81630977370436
Flowck II <flowck96@hotmail.com>
2015-01-25 13:11:53 +0100

    form

---
+++ assets/js/form.js
@@ -0,0 +1,16 @@
+/* Form */
+
+// Elementos do DOM e 
+/*var fields = document.querySelectorAll(".inputField"),
+	tamanho = fields.lenght;
+
+var vazio = null;
+
+for(var i = 0; i < tamanho; i++){
+	if(fields[i].value == ""){
+		vazio = true;
+	}else{
+
+	}
+}
+*/
