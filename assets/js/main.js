336facdb9e649ca6edefd2d86c628d34f57b0657
Flowck II <flowck96@hotmail.com>
2015-01-26 11:17:57 +0100

    Comment the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+/*
+	KiandaHUB: Landing Page
+*/
+
 // Elementos do DOM
 var BODY = document.getElementsByTagName("body"),
 	header = document.querySelector(".mainHeader");
@@ -6,12 +10,12 @@ window.addEventListener("scroll", function() {
 	//console.log(window.scrollY);
 }, false);
 
-/* Lightbox */
+/* ---- Lightbox ---- */
 
 // DOM
-var fecharBT = document.querySelector(".fechar"),
-	inscrever_me = document.querySelector(".inscricao"),
-	lightbox = document.querySelector(".form_fazerparte");
+var fecharBT = document.querySelector(".fechar");
+var	inscrever_me = document.querySelector(".inscricao");
+var	lightbox = document.querySelector(".form_fazerparte");
 
 // Abre o lightbox / formulário
 inscrever_me.addEventListener("click", function(){
@@ -23,12 +27,13 @@ fecharBT.addEventListener("click", function(){
 	lightbox.style.display = "none";
 }, false);
 
-/* Form */
+/* ---- Form ---- */
 
-// DOM
-var select_interesse = document.querySelector(".interessado"),
-	proximo = document.querySelector(".proximo");
+// Elementos DOM
+var select_interesse = document.querySelector(".interessado");
+var proximo = document.querySelector(".proximo");
 
+// Enable o botão próximo quando selecionado um item da lista
 select_interesse.addEventListener("change", function(){
 	
 	// Enable o button
@@ -36,24 +41,19 @@ select_interesse.addEventListener("change", function(){
 
 }, false);
 
-
-proximo.addEventListener("click", function(){
-
-	newForm(select_interesse.value);
-
-}, false);
-
+// Novos campos conforme a escolha do utilizador
 var newForm = function(nomeDoForm){
+
 	// DOM
-	var startup = document.querySelector(".startup"),
-		investidor = document.querySelector(".investidor"),
-		mentor = document.querySelector(".mentor"),
-		formBasico = document.querySelector(".formBasico");
+	var startup = document.querySelector(".startup");
+	var	investidor = document.querySelector(".investidor");
+	var	mentor = document.querySelector(".mentor");
+	var	formBasico = document.querySelector(".formBasico");
 
 	// Oculta o formulário básico
 	formBasico.style.display = "none";
 
-	// Mostra o form escolhido
+	// Condições para mostrar um form conforme o interesse escolhido
 	if(nomeDoForm == "startup"){
 		startup.style.display = "block";
 	}
@@ -71,4 +71,9 @@ var newForm = function(nomeDoForm){
 
 }
 
-console.log(proximo.disabled);
+// Escolha de algum item na lista de interesse
+proximo.addEventListener("click", function(){
+
+	newForm(select_interesse.value);
+
+}, false);
