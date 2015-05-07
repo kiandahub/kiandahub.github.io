3dc8aa867437f7679f28988982b86340e8f59f5a
Flowck II <flowck96@hotmail.com>
2015-05-07 13:04:12 +0100

    encapsulate all code

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,31 +1,36 @@
-// Elemento visível
-function on(){
-    c[position].style.opacity = '1'
-}
+(function(document){
+    
+    // Sets
+    var slideContainer = document.querySelector(".slide"),
+        c = slideContainer.children,
+        l = c.length - 1;
 
-// Elemento oculto
-function off(){
-    c[position].style.opacity = '0';
-}
-
-var slideContainer = document.querySelector(".slide");
-var c = slideContainer.children;
-var l = c.length - 1;
-
-var transicao = function(){
-    if(position == l){
-        off();
-        position = 0;
-        on();
-    }else{
-        off();
-        position++;
-        on();
+    // Elemento visível
+    function on(){
+        c[position].style.opacity = '1'
     }
-};
 
-position = 0;
-on();
+    // Elemento oculto
+    function off(){
+        c[position].style.opacity = '0';
+    }
 
-// Tempo até a próxima transição
-setInterval(transicao, 5000);
+    var transicao = function(){
+        if(position == l){
+            off();
+            position = 0;
+            on();
+        }else{
+            off();
+            position++;
+            on();
+        }
+    };
+
+    position = 0;
+    on();
+
+    // Tempo até a próxima transição
+    setInterval(transicao, 5000);
+
+}(document))
